0d5642328f2d6a87bfdfedfcf9413471b9a9096f
Kakise <plaid@a-smol-cat.fr>
2025-10-21 17:15:36 +0200

    tests: change back how tests are done

--- src/application/mod.rs
+++ src/application/mod.rs
@@ -75,3 +75,54 @@ where
         dry_run: cli.dry_run,
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::PathBuf;
+
+    struct MockExecutor();
+
+    impl super::CommandExecutor for MockExecutor {
+        fn run(&self, _program: &str, _args: &[&str]) -> super::Result<()> {
+            Ok(())
+        }
+    }
+
+    fn create_test_cli(
+        source: Option<&str>,
+        home_dir: Option<std::path::PathBuf>,
+        brew: bool,
+    ) -> super::Cli {
+        super::Cli {
+            source: "tests/".to_owned() + source.unwrap_or("empty-config"),
+            home: home_dir.to_owned(),
+            skip_brew: brew,
+            dry_run: true,
+        }
+    }
+
+    #[test]
+    fn test_run_with_executor() {
+        let executor = MockExecutor();
+        let result = super::run_with_executor(
+            create_test_cli(None, Some(PathBuf::from("/home/user")), true),
+            &executor,
+        );
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_run_with_executor_brew_enabled() {
+        let executor = MockExecutor();
+        let result =
+            super::run_with_executor(create_test_cli(Some("config-brew"), None, false), &executor);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_run_with_executor_no_brew() {
+        let executor = MockExecutor();
+        let result = super::run_with_executor(create_test_cli(None, None, false), &executor);
+        assert!(result.is_ok());
+    }
+}
--- src/config/mod.rs
+++ src/config/mod.rs
@@ -92,3 +92,79 @@ pub fn load_brew_spec(repo: &Path) -> Result<Option<BrewSpec>> {
     })?;
     Ok(Some(spec))
 }
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    #[test]
+    fn test_manifest_incorrect_version() {
+        let path = Path::new("tests/erroneous-config/manifest-unsupported");
+        let result = super::load_manifest(path);
+        assert!(result.is_err());
+        assert!(matches!(
+            result.unwrap_err(),
+            super::DotstrapError::UnsupportedManifestVersion { .. }
+        ));
+    }
+
+    #[test]
+    fn test_manifest_missing_templates() {
+        let path = Path::new("tests/erroneous-config/manifest-no-templates");
+        let result = super::load_manifest(path);
+        assert!(result.is_err());
+        assert!(matches!(
+            result.unwrap_err(),
+            super::DotstrapError::ManifestMissingTemplates { .. }
+        ));
+    }
+
+    #[test]
+    fn test_manifest_invalid() {
+        let path = Path::new("tests/erroneous-config/manifest-invalid");
+        let result = super::load_manifest(path);
+        assert!(result.is_err());
+        assert!(matches!(
+            result.unwrap_err(),
+            super::DotstrapError::Yaml { .. }
+        ));
+    }
+
+    #[test]
+    fn test_values_invalid() {
+        let path = Path::new("tests/erroneous-config/values-invalid");
+        let result = super::load_values(path);
+        assert!(result.is_err());
+        assert!(matches!(
+            result.unwrap_err(),
+            super::DotstrapError::Yaml { .. }
+        ));
+    }
+
+    #[test]
+    fn test_values_empty() {
+        let path = Path::new("tests/erroneous-config/values-empty");
+        let result = super::load_values(path);
+        assert!(result.is_ok());
+        assert!(result.unwrap().is_empty());
+    }
+
+    #[test]
+    fn test_values_not_found() {
+        let path = Path::new("tests/erroneous-config/values-not-found");
+        let result = super::load_values(path);
+        assert!(result.is_ok());
+        assert!(result.unwrap().is_empty());
+    }
+
+    #[test]
+    fn test_brew_spec_invalid() {
+        let path = Path::new("tests/erroneous-config/brew-invalid");
+        let result = super::load_brew_spec(path);
+        assert!(result.is_err());
+        assert!(matches!(
+            result.unwrap_err(),
+            super::DotstrapError::Yaml { .. }
+        ));
+    }
+}
